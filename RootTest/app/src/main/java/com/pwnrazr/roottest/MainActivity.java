59f9a1cdaa9c939dba6df44da7c30905cf313baa
Amir <amirz98@hotmail.com>
2020-07-19 05:15:54 +0800

    RootTest: app: Initial functioning of root
    It writes a file to /system. Magisk prompt for root request didn't show up tho

--- RootTest/app/src/main/java/com/pwnrazr/roottest/MainActivity.java
+++ RootTest/app/src/main/java/com/pwnrazr/roottest/MainActivity.java
@@ -3,6 +3,9 @@ package com.pwnrazr.roottest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView debugText = (TextView) findViewById(R.id.debugText);
+        debugText.setText("No error");
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("su -c mount -o rw,remount / && echo tested > /system/testenment");
+            p.waitFor();
+            debugText.setText(Integer.toString(p.exitValue()));
+        } catch (IOException e) {
+            debugText.setText(e.getMessage());
+        } catch (InterruptedException e ){
+            debugText.setText(e.getMessage());
+        }
     }
 }
