8700795ef489d4eca17cf55d2b23e5e431b73b22
Amir <amirz98@hotmail.com>
2020-07-19 12:08:22 +0800

    RootTest: main: Add simple toast notification

--- RootTest/app/src/main/java/com/pwnrazr/roottest/MainActivity.java
+++ RootTest/app/src/main/java/com/pwnrazr/roottest/MainActivity.java
@@ -2,8 +2,10 @@ package com.pwnrazr.roottest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         debugText.setText("No error");
 
+        Context context = getApplicationContext();
+
         Process p;
         try {
             p = Runtime.getRuntime().exec("su -c mount -o rw,remount / && echo tested > /system/testenment && rm /system/testenment && mount -o ro,remount / && cat /proc/version");
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             //debugText.setText(Integer.toString(p.exitValue()));
             //Get cmd output
             BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(p.getInputStream()));
+                new InputStreamReader(p.getInputStream()));
 
             // Grab the results
             StringBuilder log = new StringBuilder();
@@ -41,9 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
             if(p.exitValue()==0)
             {
-                debugText.setText("Device is rooted!");
+            debugText.setText("Device is rooted!");
+            Toast toast = Toast.makeText(context, "Device is rooted", Toast.LENGTH_SHORT);
+            toast.show();
             } else {
-                debugText.setText("Root was denied!");
+            debugText.setText("Root was denied!");
             }
         } catch (IOException e) {
             debugText.setText(e.getMessage());
