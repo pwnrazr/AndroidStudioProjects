1e88b861dc5bea14e97ef81c8cb0e876fe4737bf
Amir <amirz98@hotmail.com>
2020-07-22 14:02:04 +0800

    ThermalMitigationApp: main: Improve code - Use method instead

--- ThermalMitigation/app/src/main/java/com/pwnrazr/thermalmitigation/MainActivity.java
+++ ThermalMitigation/app/src/main/java/com/pwnrazr/thermalmitigation/MainActivity.java
@@ -3,6 +3,7 @@ package com.pwnrazr.thermalmitigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -12,6 +13,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String root_cmd(String command) {
+        StringBuilder log = new StringBuilder();
+        try {
+            Process cmd;
+            cmd = Runtime.getRuntime().exec(command);
+            cmd.waitFor();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(cmd.getInputStream()));
+
+            // Get output from command
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {    // Append chars from InputStream
+                log.append(line + "\n");
+            }
+        } catch (IOException | InterruptedException e) {
+            Log.e("ThermMitigationApp",e.toString());
+        }
+        return log.toString();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +44,18 @@ public class MainActivity extends AppCompatActivity {
 
         Switch thermSwitch = findViewById(R.id.thermSwitch);
 
-        try {
-            Process cmd;
-            //cmd = Runtime.getRuntime().exec("su -c mount -o rw,remount / && echo tested > /system/testenment && rm /system/testenment && mount -o ro,remount /");
-            cmd = Runtime.getRuntime().exec("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
-                    cmd.waitFor();
-            BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(cmd.getInputStream()));
+        String returnCMD = "";
+        // Get current skip thermal state on app startup
+        returnCMD = root_cmd("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
 
-            // Get current skip thermal state
-            StringBuilder log = new StringBuilder();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {    //append chars from InputStream
-                log.append(line + "\n");
-            }
-            if(log.toString().equals("1\n")) {
-                statusText.setText("Enabled");
-                thermSwitch.setChecked(true);
-            }else if(log.toString().equals("0\n")){
-                statusText.setText("Disabled");
-                thermSwitch.setChecked(false);
-            }else {
-                statusText.setText("ERROR");
-            }
-            //debugText.setText(Integer.toString(cmd.exitValue()));
-        } catch (IOException | InterruptedException e) {
-            debugText.setText(e.getMessage());
+        if(returnCMD.equals("1\n")) {
+            statusText.setText("Enabled");
+            thermSwitch.setChecked(true);
+        }else if(returnCMD.equals("0\n")){
+            statusText.setText("Disabled");
+            thermSwitch.setChecked(false);
+        }else {
+            statusText.setText("ERROR");
         }
 
         thermSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -55,72 +63,34 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b) {
                     //debugText.setText("Enabled!");
-                    try {
-                        Process cmd_modify;
-                        cmd_modify = Runtime.getRuntime().exec("su -c echo 1 > /sys/module/smb5_lib/parameters/skip_therm");
-                        cmd_modify.waitFor();
-                    } catch (IOException | InterruptedException e) {
-                        debugText.setText(e.getMessage());
-                    }
+                    root_cmd("su -c echo 1 > /sys/module/smb5_lib/parameters/skip_therm");
+
                     //verify that it is set
-                    try {
-                        Process cmd;
+                    String returnCMD = "";
+                    returnCMD = root_cmd("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
 
-                        cmd = Runtime.getRuntime().exec("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
-                        cmd.waitFor();
-                        BufferedReader bufferedReader = new BufferedReader(
-                                new InputStreamReader(cmd.getInputStream()));
-
-                        // Get current skip thermal state
-                        StringBuilder log = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {    //append chars from InputStream
-                            log.append(line + "\n");
-                        }
-                        if(log.toString().equals("1\n")) {
-                            statusText.setText("Enabled");
-                        }else if(log.toString().equals("0\n")){
-                            statusText.setText("Disabled");
-                        }else {
-                            statusText.setText("ERROR");
-                        }
-                    } catch (IOException | InterruptedException e) {
-                        debugText.setText(e.getMessage());
+                    if(returnCMD.equals("1\n")) {
+                        statusText.setText("Enabled");
+                    }else if(returnCMD.equals("0\n")){
+                        statusText.setText("Disabled");
+                    }else {
+                        statusText.setText("ERROR");
                     }
 
                 } else {
                     //debugText.setText("Disabled");
-                    try {
-                        Process cmd_modify;
-                        cmd_modify = Runtime.getRuntime().exec("su -c echo 0 > /sys/module/smb5_lib/parameters/skip_therm");
-                        cmd_modify.waitFor();
-                    } catch (IOException | InterruptedException e) {
-                        debugText.setText(e.getMessage());
-                    }
+                    root_cmd("su -c echo 0 > /sys/module/smb5_lib/parameters/skip_therm");
+
                     //verify that it is set
-                    try {
-                        Process cmd;
+                    String returnCMD = "";
+                    returnCMD = root_cmd("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
 
-                        cmd = Runtime.getRuntime().exec("su -c cat /sys/module/smb5_lib/parameters/skip_therm");
-                        cmd.waitFor();
-                        BufferedReader bufferedReader = new BufferedReader(
-                                new InputStreamReader(cmd.getInputStream()));
-
-                        // Get current skip thermal state
-                        StringBuilder log = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {    //append chars from InputStream
-                            log.append(line + "\n");
-                        }
-                        if(log.toString().equals("1\n")) {
-                            statusText.setText("Enabled");
-                        }else if(log.toString().equals("0\n")){
-                            statusText.setText("Disabled");
-                        }else {
-                            statusText.setText("ERROR");
-                        }
-                    } catch (IOException | InterruptedException e) {
-                        debugText.setText(e.getMessage());
+                    if(returnCMD.equals("1\n")) {
+                        statusText.setText("Enabled");
+                    }else if(returnCMD.equals("0\n")){
+                        statusText.setText("Disabled");
+                    }else {
+                        statusText.setText("ERROR");
                     }
                 }
             }
